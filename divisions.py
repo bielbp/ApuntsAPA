adffcabb2a284624add77fa72a103c85740837d8
Biel Bernal Pratdesaba <biel.bernal@estudiantat.upc.edu>
2025-03-19 19:52:11 +0100

    Classe 19/03/25

---
+++ divisions.py
@@ -0,0 +1,10 @@
+"""
+Aquesta es la cadena de documentació del mòdul
+"""
+
+def dividesidivisible(a, b,):
+    if a % b == 0:
+        return a // b
+
+def quocientmodul(a, b):
+    return a // b, a % b
